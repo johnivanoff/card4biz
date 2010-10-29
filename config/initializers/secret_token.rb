d8ab1e2d0d82828bd3d895970544c08a9b6274fa
John Ivanoff <john.ivanoff@gmail.com>
2010-10-29 14:11:43 -0500

    card4biz  Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Card4biz::Application.config.secret_token = 'd75cc28cc7bac8c8dbcddbfdf064349c9d4aae71c03da3bd281af9e3260b3d18f39dfed3cee7154a6d68fdfd222926580062d76dae0e937bd39d1fcd2820c5d0'
