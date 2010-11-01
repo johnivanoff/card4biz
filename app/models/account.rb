7d2628fb3c15406553ff2271a30b4c177e2e25ad
John Ivanoff <john.ivanoff@gmail.com>
2010-11-01 16:56:51 -0500

    added validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,12 @@
 class Account < ActiveRecord::Base
   has_many :contacts
   
+  validates_presence_of :name
+  validates :website,   :format => {
+    :with =>    /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix,
+    :message => 'must be a complete URL'
+  }, :allow_blank => true
+  
   def full_address
     address + " " + city + " " + state + ", " + zip_code
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ActiveRecord::Base
   belongs_to :account
   
+  validates_presence_of :first_name, :last_name
+  
   def full_name
     first_name + " " + last_name
   end
