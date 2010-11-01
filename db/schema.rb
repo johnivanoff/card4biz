cc3727299d89639f4e696fdbaa5ca5405acc104f
John Ivanoff <john.ivanoff@gmail.com>
2010-11-01 16:58:00 -0500

    update before going home. crappy notes, eh?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101029205120) do
+ActiveRecord::Schema.define(:version => 20101101210523) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20101029205120) do
     t.string   "zip_code"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "web_site"
   end
 
   create_table "contacts", :force => true do |t|
