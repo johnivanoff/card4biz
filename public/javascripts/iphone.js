4c6b9ccbbd958d5166e408eb3267c401d5bf98af
John Ivanoff <john.ivanoff@gmail.com>
2010-11-01 19:50:52 -0500

    tweak files

--- public/javascripts/iphone.js
+++ public/javascripts/iphone.js
@@ -1,4 +1,4 @@
-if (window.innerWidth && window.innerWidth <= 480) {
+if (window.innerWidth && window.innerWidth <= 981) {
     $(document).ready(function(){ 
         $('#header ul').addClass('hide'); 
         $('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');
@@ -7,5 +7,4 @@ if (window.innerWidth && window.innerWidth <= 480) {
         $('#header ul').toggleClass('hide'); 
         $('#header .leftButton').toggleClass('pressed');
     }
-  scrollTo(0,0);
 }
