a256fd8590c3f3b81dcd03b326d42a491413b2b1
John Ivanoff <john.ivanoff@gmail.com>
2010-11-01 22:16:31 -0500

    got hijack to work

--- public/javascripts/iphone.js
+++ public/javascripts/iphone.js
@@ -1,11 +1,51 @@
 if (window.innerWidth && window.innerWidth <= 981) {
     $(document).ready(function(){ 
         $('#header ul').addClass('hide'); 
-        $('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');
+        $('#header').append('<div class="rightButton" onclick="toggleMenu()">Menu</div>');
     }); 
     function toggleMenu() {
         $('#header ul').toggleClass('hide'); 
-        $('#header .leftButton').toggleClass('pressed');
+        $('#header .rightButton').toggleClass('pressed');
     }
 }
-scrollTo(0,0)
+
+var hist = [];
+var startUrl = '/';
+$(document).ready(function(){
+    loadPage(startUrl);
+});
+function loadPage(url) {
+    $('body').append('<div id="progress">Loading...</div>');
+    scrollTo(0,0);
+    if (url == startUrl) {
+        var element = ' #header ul';
+    } else {
+        var element = ' #content';
+    }
+    $('#container').load(url + element, function(){
+        var title = $('h1').html() || 'ugh!';
+        $('h1').html(title); 
+		/*
+		$('h1').remove();
+        */
+		$('.leftButton').remove();
+        hist.unshift({'url':url, 'title':title});
+        if (hist.length > 1) {
+            $('#header').append('<div class="leftButton">'+hist[1].title+'</div>');
+            $('#header .leftButton').click(function(e){
+                $(e.target).addClass('clicked');
+                var thisPage = hist.shift();
+                var previousPage = hist.shift();
+                loadPage(previousPage.url);
+            });
+        }
+        $('#container a').click(function(e){
+            var url = e.target.href;
+            if (url.match(/127.0.0.1:3000/)) {
+                e.preventDefault();
+                loadPage(url);
+            }
+        });
+        $('#progress').remove();
+    });
+}
